4a8425fa1f750728456bf3252b6259e587fbbfa5
PetarKombow <146661533+PetarKombow@users.noreply.github.com>
2023-10-01 20:04:07 +0300

    Add files via upload

---
+++ Domashna/Domashna/Familiq.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Domashna
+{
+    internal class Familiq
+    {
+        public string Name;
+        public int Age;
+
+        public Familiq(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public class Family
+        {
+            public List<Familiq> members;
+
+            public Family()
+            {
+                members = new List<Familiq>();
+            }
+
+            public void AddMember(Familiq hora)
+            {
+                members.Add(hora);
+            }
+            public void PrintFamily()
+            {
+                members.Sort((h1, h2) => string.Compare(h1.Name, h2.Name));
+                foreach (var h in members)
+                {
+                    if (h.Age >= 30) // izkluchva ako sa pod 30 godini
+                    {
+                        Console.WriteLine($"Name: {h.Name}, Age: {h.Age}");
+                    }
+                }
+            }
+        }
+
+    }
+    }
---
+++ Domashna/Domashna/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Domashna.Familiq;
+
+namespace Domashna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of family members: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Enter the name of person {i + 1}: ");
+                string name = Console.ReadLine();
+
+                Console.Write($"Enter the age of person {i + 1}: ");
+                int age = int.Parse(Console.ReadLine());
+
+                Familiq person = new Familiq(name, age);
+                family.AddMember(person);
+            }
+            /*sortirani po ime
+            Console.WriteLine("Ot semeistvoto(sortirani po ime) : ");
+            family.PrintFamily();*/
+            //sortirane po vuzrast ako sa pod 30
+            Console.WriteLine("Ot semeistvoto(sortirani po ime) : ");
+            family.PrintFamily();
+        }
+    }
+}
